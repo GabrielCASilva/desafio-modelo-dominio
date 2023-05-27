a466b285d6ce0b73bf0adcc38ada0be2bb12b6d9
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2023-05-27 17:50:54 -0300

    java class participante

--- src/main/java/com/desafio/modelodominio/entities/Bloco.java
+++ src/main/java/com/desafio/modelodominio/entities/Bloco.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +25,10 @@ public class Bloco {
 
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant fim;
+
+	@ManyToOne
+	@JoinColumn(name = "atividade_id")
+	private Atividade atividade;
 
 	public Bloco() {
 	}
@@ -60,6 +66,10 @@ public class Bloco {
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+
+	public Atividade getAtividade() {
+		return atividade;
 	}
 
 	@Override
